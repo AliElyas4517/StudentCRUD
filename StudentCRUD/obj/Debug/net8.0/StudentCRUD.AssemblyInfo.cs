a9fc8770353b9a6fb3f7e76d4da194d15777547c
AliElyas4517 <aliilyas4517@gmail.com>
2025-02-09 09:28:03 +0500

    Latest CRUD

--- StudentCRUD/obj/Debug/net8.0/StudentCRUD.AssemblyInfo.cs
+++ StudentCRUD/obj/Debug/net8.0/StudentCRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentCRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+136a673b68ceebaeda082b0290b504b5363b9165")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentCRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentCRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
